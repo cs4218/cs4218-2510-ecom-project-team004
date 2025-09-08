63223a483eab8b9cb3fdcd4f59c4681e0db7e324
Evelyne Law <lawruixi@gmail.com>
2025-09-08 19:52:13 +0800

    Added test for axios.get()

--- client/src/components/Routes/Private.test.js
+++ client/src/components/Routes/Private.test.js
@@ -11,19 +11,36 @@ import PrivateRoute from './Private';
 jest.mock('axios');
 
 jest.mock('../../context/auth', () => ({
-    useAuth: jest.fn(() => [null, jest.fn()]) // Mock useAuth hook to return null state and a mock function for setAuth
+    useAuth: jest.fn(() => [
+        { user: null, token: "A" }, jest.fn() // Null user and non-empty token for authentication purposes.
+    ]) // Mock useAuth hook to return null state and a mock function for setAuth
   }));
 
 describe('Private Route', () => {
-    // it('should call axios.get()?')
+    it('should call axios.get', async() => {
+        // Mock axios.get.
+        const mockAuth = { data: { ok: false } }
+        axios.get.mockResolvedValueOnce(mockAuth);
+
+        render(
+          <MemoryRouter initialEntries={['/dashboard']}>
+            <Routes>
+              <Route path="/dashboard" element={<PrivateRoute />} />
+            </Routes>
+          </MemoryRouter>
+        );
+
+        expect(axios.get.mock.calls).toHaveLength(1);
+    })
 
     it('should redirect when not authenticated', async() => {
         // Mock axios.get to return unauthenticated.
-        const mockAuth = { ok: false }
+        const mockAuth = { data: { ok: false } }
 
         axios.get.mockResolvedValueOnce(mockAuth);
 
         // Check if Spinner is returned in the document.
+        // We can check if it contains the word "redirecting".
         const { getByText } = render(
           <MemoryRouter initialEntries={['/dashboard']}>
             <Routes>
