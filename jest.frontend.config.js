763b768718371f38667e9e41b58b87be1ef47d8c
Evelyne Law <lawruixi@gmail.com>
2025-09-12 15:10:47 +0800

    Added Dashboard tests.

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -22,13 +22,19 @@ export default {
   // only run these tests
   testMatch: [
       "<rootDir>/client/src/pages/Auth/*.test.js",
+      "<rootDir>/client/src/pages/user/Dashboard.test.js", 
       "<rootDir>/client/src/components/UserMenu.test.js",
       "<rootDir>/client/src/components/Routes/Private.test.js"
   ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**", "client/src/components/UserMenu.js", "client/src/components/Routes/Private.js"],
+  collectCoverageFrom: [
+      "client/src/pages/Auth/**", 
+      "client/src/pages/user/Dashboard.js", 
+      "client/src/components/UserMenu.js", 
+      "client/src/components/Routes/Private.js"
+  ],
   coverageThreshold: {
     global: {
       lines: 100,
