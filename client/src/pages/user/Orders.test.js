93a62568ae43c295a2f9ddb7891dd2b4c4ea5df7
Evelyne Law <lawruixi@gmail.com>
2025-09-17 12:00:41 +0800

    Finished orders test

--- client/src/pages/user/Orders.test.js
+++ client/src/pages/user/Orders.test.js
@@ -65,8 +65,72 @@ describe("Orders component", () => {
         expect(getByText("Success")).toBeInTheDocument();
     })
 
+    it("should call API and render products", async () => {
+        axios.get.mockResolvedValueOnce({ data: mockOrders });
+
+        const { getByText, getByAltText } = render(
+            <MemoryRouter>
+                <Orders/>
+            </MemoryRouter>
+        );
+
+        await waitFor(() => {
+            // Wait for the first order (and therefore all other orders) to be rendered.
+            expect(getByText("#")).toBeInTheDocument();
+        });
+
+        expect(axios.get).toHaveBeenCalledWith("/api/v1/auth/orders");
+        expect(getByText("Test Product")).toBeInTheDocument();
+        expect(getByText("Used for testing.")).toBeInTheDocument();
+        expect(getByText(/500/)).toBeInTheDocument();
+
+        // Test if image is there.
+        const image = getByAltText("Test Product");
+        expect(image).toBeInTheDocument();
+        expect(image).toHaveAttribute("src", "/api/v1/product/product-photo/abcdef")
+    })
+
     it("should call API and not render any orders if empty", async () => {
         axios.get.mockResolvedValueOnce({ data: [] });
+
+        const { getByText } = render(
+            <MemoryRouter>
+                <Orders/>
+            </MemoryRouter>
+        );
+
+        await waitFor(() => {
+            // Wait for the "all orders" text to be rendered.
+            expect(getByText("All Orders")).toBeInTheDocument();
+        });
+
+        // There should be no children of the parent of this element.
+        const all_orders_parent = getByText("All Orders").parentNode;
+        expect(all_orders_parent.children).toHaveLength(1);
+    })
+
+    it("should call API and not render any products if empty", async () => {
+        const mockOrdersNoProducts = [Object.assign(mockOrders[0], { products: [] })]
+        axios.get.mockResolvedValueOnce({ data: mockOrdersNoProducts });
+
+        const { getByText, queryByText } = render(
+            <MemoryRouter>
+                <Orders/>
+            </MemoryRouter>
+        );
+
+        await waitFor(() => {
+            // Wait for the "all orders" text to be rendered.
+            expect(getByText("All Orders")).toBeInTheDocument();
+        });
+
+        expect(queryByText(/Price/)).not.toBeInTheDocument();
+    })
+
+    it("should not render orders if there is an error calling the API", async () => {
+        axios.get.mockImplementation(() => {
+            throw new Error();
+        });
 
         const { getByText } = render(
             <MemoryRouter>
