ab35d7704abac5cd231c959c9926d8f9e3a846a4
Daphne789 <daphneshaine933@gmail.com>
2025-09-18 14:48:11 +0800

    Add GenAI usage acknowledgement

--- client/src/pages/admin/Products.test.js
+++ client/src/pages/admin/Products.test.js
@@ -1,3 +1,4 @@
+// The tests below are generated with help from GenAI
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
