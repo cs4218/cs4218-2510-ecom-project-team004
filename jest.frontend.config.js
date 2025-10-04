bf24b6de0034fd8010b6452962c298e368e7d94a
foo-lin-xuan <linxuan1994@gmail.com>
2025-10-04 10:04:16 +0800

    add About page test

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -21,6 +21,7 @@ export default {
 
   // only run these tests
   testMatch: [
+    "<rootDir>/client/src/components/*.test.js",
     "<rootDir>/client/src/pages/admin/*.test.js",
     "<rootDir>/client/src/pages/Auth/*.test.js",
     "<rootDir>/client/src/pages/*.test.js",
@@ -29,8 +30,10 @@ export default {
   // jest code coverage
   collectCoverage: true,
   collectCoverageFrom: [
+    "client/src/components/Footer.js",
     "client/src/pages/admin/Users.js",
     "client/src/pages/Auth/**",
+    "client/src/pages/About.js",
     "client/src/pages/Contact.js",
     "client/src/pages/Policy.js",
   ],
