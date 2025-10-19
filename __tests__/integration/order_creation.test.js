d6bc018e36ce7209fb7e69ee81fd232eae86ea51
Evelyne Law <lawruixi@gmail.com>
2025-10-19 19:58:02 +0800

    Added order creation integration test.

--- __tests__/integration/order_creation.test.js
+++ __tests__/integration/order_creation.test.js
@@ -2,16 +2,21 @@ import mongoose from "mongoose";
 import { hashPassword } from "../../helpers/authHelper";
 import request from "supertest";
 import userModel from "../../models/userModel";
+import productModel from "../../models/productModel";
+import categoryModel from "../../models/categoryModel";
 import app from "../../server.js";
 import {BraintreeGateway, Environment} from "braintree";
+import braintree from "braintree";
 
 jest.mock("braintree", () => {
+    const mockSale = jest.fn();
     return {
         BraintreeGateway: jest.fn().mockImplementation(() => ({
             clientToken: { generate: jest.fn() },
-            transaction: { sale: jest.fn() }
+            transaction: { sale: mockSale }
         })),
-        Environment: { Sandbox: "sandbox" }
+        Environment: { Sandbox: "sandbox" },
+        mockSale: mockSale
     }
 })
 
@@ -19,6 +24,10 @@ describe("Users can create and view their orders", () => {
     let userToken;
     let hashedPassword;
     let user;
+    let testCategory;
+
+    let testProduct1;
+    let testProduct2;
 
     beforeAll(async () => {
         hashedPassword = await hashPassword("Password123");
@@ -31,6 +40,31 @@ describe("Users can create and view their orders", () => {
             phone: "81234567",
             answer: "Test Answer",
             role: 1
+        })
+
+        testCategory = await categoryModel.create({
+            name: "Test Category",
+            slug: "test-category"
+        })
+
+        testProduct1 = await productModel.create({
+            name: "Test 1",
+            slug: "test-1",
+            description: "Test Description 1",
+            price: 100,
+            category: testCategory._id,
+            quantity: 1,
+            shipping: false
+        })
+
+        testProduct2 = await productModel.create({
+            name: "Test 2",
+            slug: "test-2",
+            description: "Test Description 2",
+            price: 200,
+            category: testCategory._id,
+            quantity: 2,
+            shipping: true
         })
 
         const loginRes = await request(app).post("/api/v1/auth/login").send({
@@ -56,7 +90,27 @@ describe("Users can create and view their orders", () => {
     })
 
     test("User can create and view an order.", async () => {
-        const res = await request(app).post("/api/v1/product/braintree/payment").set("Authorization", userToken);
-        console.log(res.body);
-    })
+        const fakeResult = { success: true }
+        braintree.mockSale.mockImplementation((data, callback) => callback(null, fakeResult));
+
+        // Create an order with testProduct1 and testProduct2.
+        const res = await request(app).post("/api/v1/product/braintree/payment").set("Authorization", userToken).send({
+            nonce: "123",
+            cart: [ testProduct1, testProduct2 ].map((p) => ({ _id: p._id, price: p.price }))
+        });
+        
+        expect(braintree.mockSale).toHaveBeenCalled();
+        expect(res.statusCode).toBe(200);
+        expect(res.body.ok).toBe(true);
+
+        // View order as user.
+        const res2 = await request(app).get("/api/v1/auth/orders").set("Authorization", userToken);
+
+        expect(res2.statusCode).toBe(200);
+        expect(res2.body).toHaveLength(1);
+        expect(res2.body[0].products).toHaveLength(2);
+        expect(res2.body[0].products[0]._id).toBe(testProduct1._id.toString());
+        expect(res2.body[0].products[1]._id).toBe(testProduct2._id.toString());
+        expect(res2.body[0].buyer._id).toBe(user._id.toString());
+    }, 30000)
 })
